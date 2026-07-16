4a65c3bff5eacfa05d12aec5d6ef47f664702d9b
worldprotocolacademy-code <worldprotocolacademy@gmail.com>
2026-07-16 19:03:16 +0200

    Wire v35.1.2 academic candidate to staging

--- workers/virtual-sande-v35.1-staging/src/index.mjs
+++ workers/virtual-sande-v35.1-staging/src/index.mjs
@@ -1,3 +1,3 @@
-import worker from '../../../tools/virtual-sande/wpa-protocol-bot-v35.1-safe-entrypoint.mjs';
+import worker from '../../../tools/virtual-sande/wpa-protocol-bot-v35.1.2-academic-entrypoint.mjs';
 
 export default worker;
