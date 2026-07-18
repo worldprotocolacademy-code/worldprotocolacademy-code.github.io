5a9ef76905e525a984e5ecc78a242a6a6962553e
worldprotocolacademy-code <worldprotocolacademy@gmail.com>
2026-07-18 23:42:24 +0200

    Add interactive WPA programme families catalogue

---
+++ scripts/wpa-programme-families-interactive.js
@@ -0,0 +1,11 @@
+/* WPA Programme Families interactive catalogue v1.0
+ * Local-only educational navigation. No enrolment, payment, assessment or credential action.
+ */
+(function(){
+  'use strict';
+  if(window.WPA_PROGRAMME_FAMILIES_INTERACTIVE_LOADED)return;
+  window.WPA_PROGRAMME_FAMILIES_INTERACTIVE_LOADED=true;
+
+  var families=[
+    {
+      id:'protocol',icon
